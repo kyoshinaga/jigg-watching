2661d4052d6274c95eb43a7b2133bb900b245aef
tomeken-yoshinaga <ubuntu@ip-172-31-18-86.ap-northeast-1.compute.internal>
2016-06-21 17:14:46 +0900

    [update] Scaling down the traning for spped-up.

--- test.sh
+++ test.sh
@@ -5,27 +5,36 @@ PARSER_MODEL_NAME=parser.ser.gz
 BEAM_SIZE=4
 
 cd jigg-develop
+
+if [ -e ./result ]; then
+				echo "result directory exist."
+else
+				mkdir result
+fi
+
 ./bin/sbt assembly
 cd target
 
 # Super tagger training
 java -Xmx4g -cp *.jar jigg.nlp.ccg.TrainSuperTagger \
 				-bank.lang ja -bank.dir /data/ccgbank-20150216/ \
-				-model /tmp/$TAGGER_MODEL_NAME
+				-bank.trainSize 100 \
+				-model ../result/$TAGGER_MODEL_NAME
 
 # Parser training
 java -Xmx5g -cp *.jar jigg.nlp.ccg.TrainParser \
-				-taggerModel /tmp/$TAGGER_MODEL_NAME \
-				-model /tmp/$PARSER_MODEL_NAME \
+				-taggerModel ../result/$TAGGER_MODEL_NAME \
+				-model ../result/$PARSER_MODEL_NAME \
 				-beam $BEAM_SIZE \
 				-bank.lang ja\
+				-bank.trainSize 100 \
 				-bank.dir /data/ccgbank-20150216
 
 # Evaluation
 java -Xmx4g -cp *.jar jigg.nlp.ccg.EvalJapaneseParser \
-				-model /tmp/$PARSER_MODEL_NAME \
+				-model ../result/$PARSER_MODEL_NAME \
 				-decoder.beam $BEAM_SIZE \
-				-output /tmp/ \
+				-output ../result/ \
 				-bank.dir /data/ccgbank-20150216 \
 				-useTest true \
 				-cabocha /data/test.cabocha
@@ -34,6 +43,4 @@ java -Xmx4g -cp *.jar jigg.nlp.ccg.EvalJapaneseParser \
 java -cp *.jar jigg.nlp.ccg.Cabocha2CoNLL \
 				-ccgBank /data/ccgbank-20150216/test.ccgbank \
 				-cabocha /data/test.cabocha \
-				-output /tmp/test.conll
-
-ls /tmp
+				-output ../result/test.conll
