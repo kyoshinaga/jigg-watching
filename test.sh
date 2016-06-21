2161190df10df20c87c709ed496e1bdc9f08431a
tomeken-yoshinaga <ubuntu@ip-172-31-18-86.ap-northeast-1.compute.internal>
2016-06-21 13:31:33 +0900

    [update] Build, Training, Evaluate

--- test.sh
+++ test.sh
@@ -1,16 +1,39 @@
 #!/bin/sh -e
 
+TAGGER_MODEL_NAME=tagger.ser.gz
+PARSER_MODEL_NAME=parser.ser.gz
+BEAM_SIZE=8
+
 cd jigg-develop
-sbt assembly
+./bin/sbt assembly
 cd target
 
 # Super tagger training
-java -Xmx8g -cp *.jar jigg.nlp.ccg.TrainSuperTagger \
+java -Xmx4g -cp *.jar jigg.nlp.ccg.TrainSuperTagger \
 				-bank.lang ja -bank.dir /data/ccgbank-20150216/ \
-				-model /tmp/tagger.ser.gz
-cd /tmp
-ls -la
+				-model /tmp/$TAGGER_MODEL_NAME
 
 # Parser training
+java -Xmx5g -cp *.jar jigg.nlp.ccg.TrainParser \
+				-taggerModel /tmp/$TAGGER_MODEL_NAME \
+				-model /tmp/$PARSER_MODEL_NAME \
+				-beam $BEAM_SIZE \
+				-bank.lang ja\
+				-bank.dir /data/ccgbank-20150216
 
-# Convert to CoNLL format.
+# Evaluation
+java -Xmx4g -cp *.jar jigg.nlp.ccg.EvalJapaneseParser \
+				-model /tmp/$PARSER_MODEL_NAME \
+				-decoder.beam $BEAM_SIZE \
+				-output /tmp/ \
+				-bank.dir /data/ccgbank-20150216 \
+				-useTest true \
+				-cabocha /data/test.cabocha
+
+# Convert to CoNLL format
+java -cp *.jar jigg.nlp.ccg.Cabocha2CoNLL \
+				-ccgBank /data/ccgbank-20150216/test.ccgbank \
+				-cabocha /data/test.cabocha \
+				-output /tmp/test.conll
+
+ls /tmp
