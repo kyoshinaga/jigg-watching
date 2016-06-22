cb21b23d42e31a5ef0ccb34a694405cd52010ff3
tomeken-yoshinaga <ubuntu@ip-172-31-18-86.ap-northeast-1.compute.internal>
2016-06-22 16:58:29 +0900

    [update] Change result directory to /work/result

--- test.sh
+++ test.sh
@@ -6,10 +6,10 @@ BEAM_SIZE=4
 
 cd jigg-develop
 
-if [ -e ./result ]; then
+if [ -e /work/result ]; then
 				echo "result directory exist."
 else
-				mkdir result
+				mkdir /work/result
 fi
 
 ./bin/sbt assembly
@@ -19,12 +19,12 @@ cd target
 java -Xmx4g -cp *.jar jigg.nlp.ccg.TrainSuperTagger \
 				-bank.lang ja -bank.dir /data/ccgbank-20150216/ \
 				-bank.trainSize 100 \
-				-model ../result/$TAGGER_MODEL_NAME
+				-model /work/result/$TAGGER_MODEL_NAME
 
 # Parser training
 java -Xmx5g -cp *.jar jigg.nlp.ccg.TrainParser \
-				-taggerModel ../result/$TAGGER_MODEL_NAME \
-				-model ../result/$PARSER_MODEL_NAME \
+				-taggerModel /work/result/$TAGGER_MODEL_NAME \
+				-model /work/result/$PARSER_MODEL_NAME \
 				-beam $BEAM_SIZE \
 				-bank.lang ja\
 				-bank.trainSize 100 \
@@ -32,17 +32,17 @@ java -Xmx5g -cp *.jar jigg.nlp.ccg.TrainParser \
 
 # Evaluation
 java -Xmx4g -cp *.jar jigg.nlp.ccg.EvalJapaneseParser \
-				-model ../result/$PARSER_MODEL_NAME \
+				-model /work/result/$PARSER_MODEL_NAME \
 				-decoder.beam $BEAM_SIZE \
-				-output ../result/ \
+				-output /work/result/ \
 				-bank.dir /data/ccgbank-20150216 \
 				-useTest true \
 				-cabocha /data/test.cabocha \
 				2>&1 | awk '/:/{print}' \
-				| tee ../result/eval.log
+				| tee /work/result/eval.log
 
 # Convert to CoNLL format
 java -cp *.jar jigg.nlp.ccg.Cabocha2CoNLL \
 				-ccgBank /data/ccgbank-20150216/test.ccgbank \
 				-cabocha /data/test.cabocha \
-				-output ../result/test.conll
+				-output /work/result/test.conll
