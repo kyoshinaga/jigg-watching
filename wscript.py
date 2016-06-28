3fbdc4c6ea8842c1fda9cb2a01646213c778348f
tomeken-yoshinaga <kenta-yoshinaga@aist.go.jp>
2016-06-28 17:44:11 +0900

    [update] Modify argument name.

--- wscript.py
+++ wscript.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Generate evaluation result as xml/jsonj')
 
-    parser.add_argument('input eval file', metavar='INPUT_EVAL_FILE', type=str,\
+    parser.add_argument('inputEvalFile', metavar='INPUT_EVAL_FILE', type=str,\
                         help='Input file name of evalulation resuit, default ./eval.log',\
                         default="./eval.log")
     parser.add_argument('beam', metavar='BEAM',type=int,\
@@ -17,4 +17,5 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
+    print(args.inputEvalFile)
     print(args.beam)
